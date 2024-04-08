4ee62c63eae65cc6f615d56311bfcd1c7a2c3316
ViktorVelizarov <VVVelizarov18@codingburgas.bg>
2024-04-08 18:33:45 +0200

    connect to DB and get all the houses from table

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,28 @@
 const express = require("express")    
-const mysql = require("mysql")    
+const mysql = require('mysql2') 
 const cors = require("cors")    
 
 const app = express()
 app.use(cors())
 
+const db = mysql.createConnection({
+    host: "localhost",
+    user: 'root',
+    password: '',
+    port    :'4306',
+    database: 'properties'
+})
+
 app.get('/', (re, res) => {
     return res.json ("From Backend  side")
+})
+
+app.get('/houses', (req, res) => {
+    const sql = "SELECT * FROM wp_postmeta";
+    db.query(sql, (err, data) => {
+        if(err) return res.json(err);
+        return res.json(data);
+    })
 })
 
 app.listen(3000, () => {
